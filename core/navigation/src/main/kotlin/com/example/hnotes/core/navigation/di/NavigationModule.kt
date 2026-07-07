18ddcdff9405a4bf3fabcab590c705ff25fcdf0e
GATTAL Hamza <hmzgtl16@gmail.com>
2026-07-07 16:32:19 +0100

    Rename `NavigatorModule` to `NavigationModule` and provide `Navigator3` dependencies

--- core/navigation/src/main/kotlin/com/example/hnotes/core/navigation/di/NavigationModule.kt
+++ core/navigation/src/main/kotlin/com/example/hnotes/core/navigation/di/NavigationModule.kt
@@ -1,6 +1,8 @@
 package com.example.hnotes.core.navigation.di
 
 import com.example.hnotes.core.navigation.Navigator
+import com.example.hnotes.core.navigation.Navigator3
+import com.example.hnotes.core.navigation.Navigator3Impl
 import com.example.hnotes.core.navigation.NavigatorImpl
 import dagger.Module
 import dagger.Provides
@@ -10,9 +12,13 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-internal object NavigatorModule {
+internal object NavigationModule {
 
     @Provides
     @Singleton
     internal fun bindsNavigator(): Navigator = NavigatorImpl()
+
+    @Provides
+    @Singleton
+    internal fun bindsNavigator3(): Navigator3 = Navigator3Impl()
 }
