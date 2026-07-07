9d48c2f489782a7be3108d01e10f2521956a8b59
GATTAL Hamza <hmzgtl16@gmail.com>
2026-07-07 15:16:46 +0100

    Add Notes feature API module
    - Create `NotesNavKey` for navigation support.
    - Add `AndroidManifest.xml` and Gradle configuration for the new module.
    - Register `:feature:notes:api` in `settings.gradle.kts`.

---
+++ feature/notes/api/src/main/kotlin/com/example/hnotes/feature/notes/api/navigation/NotesNavKey.kt
@@ -0,0 +1,8 @@
+package com.example.hnotes.feature.notes.api.navigation
+
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+object NotesNavKey: NavKey {
+}
