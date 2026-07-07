8675456334114ae93177c0f448ddc6882664da4d
GATTAL Hamza <hmzgtl16@gmail.com>
2026-07-07 14:25:34 +0100

    Add NavigatorTest to core-navigation module
    - Create `NavigatorTest` class to test `Navigator3` functionality.
    - Implement setup logic using `NavigationState` and `NavBackStack` from Navigation3.

---
+++ core/navigation/src/test/kotlin/com/example/hnotes/core/navigation/NavigatorTest.kt
@@ -0,0 +1,28 @@
+package com.example.hnotes.core.navigation
+
+import androidx.navigation3.runtime.NavBackStack
+import androidx.navigation3.runtime.NavKey
+import org.junit.Before
+
+class NavigatorTest {
+
+    private lateinit var navigationState: NavigationState
+    private lateinit var navigator: Navigator3
+
+    @Before
+    fun setup() {
+        val startKey = TestKeyFirst
+        val backStack = NavBackStack<NavKey>(startKey)
+
+        navigationState = NavigationState(
+            startKey = startKey,
+            backStack = backStack,
+        )
+        navigator = Navigator3(navigationState)
+    }
+
+
+}
+
+private object TestKeyFirst : NavKey
+private object TestKeySecond : NavKey
