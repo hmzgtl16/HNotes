95a45cc476b022b69375934498f75956f3ebd821
GATTAL Hamza <hmzgtl16@gmail.com>
2026-07-09 20:05:41 +0100

    Add Search feature implementation module
    - Configure `build.gradle.kts` and `AndroidManifest.xml` for the implementation module.
    - Register `:feature:search:impl` in `settings.gradle.kts`.

---
+++ feature/search/impl/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    alias(libs.plugins.hnotes.android.feature.impl)
+    alias(libs.plugins.hnotes.android.library.compose)
+}
+
+android {
+    namespace = "com.example.hnotes.feature.search.impl"
+}
+
+dependencies {
+
+}
